1d3aad2df4a1e40c54a130dec612ac98ebb3a19a
52617365 <rasengithub@protonmail.com>
2023-07-23 12:26:24 +0300

    auto completion lsp etc

--- init.lua
+++ init.lua
@@ -1,14 +1,16 @@
+local vim = vim
+
 vim.g.mapleader = ";"
 vim.cmd("set clipboard=unnamedplus")
+vim.cmd("set nowrap")
 vim.opt.ignorecase = true
 vim.opt.hlsearch = false
 vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+-- vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.bo.softtabstop = 2
-vim.opt.showmatch = false
+-- im.bo.softtabstop = 2
+-- vim.opt.showmatch = false
 vim.opt.number = true
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
@@ -22,12 +24,45 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+local M = {
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lua",
+	},
+}
+
+M.config = function()
+	local cmp = require("cmp")
+	vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+	cmp.setup({
+		mapping = cmp.mapping.preset.insert({
+      ["<C-k>"] = cmp.mapping.select_prev_item(),
+      ["<C-j>"] = cmp.mapping.select_next_item(),
+			["<C-b>"] = cmp.mapping.scroll_docs(-4),
+			["<C-f>"] = cmp.mapping.scroll_docs(4),
+			["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		}),
+		sources = cmp.config.sources({
+			{ name = "nvim_lsp" },
+			{ name = "nvim_lua" },
+			{ name = "buffer" },
+			{ name = "path" },
+		}),
+	})
+end
+
 plugins = {
   {'nvim-telescope/telescope.nvim', tag = '0.1.1', dependencies = { 'nvim-lua/plenary.nvim' }},
   {'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
-  {"catppuccin/nvim", name = "catppuccin", priority = 1000},
+  {"Shatur/neovim-ayu"},
   {"numToStr/Comment.nvim"},
+  {"williamboman/mason.nvim", build = ":MasonUpdate"},
+  {"williamboman/mason-lspconfig.nvim"},
+  {"neovim/nvim-lspconfig"},
+  M,
 }
 
 require("lazy").setup(plugins)
@@ -35,9 +70,32 @@ require('telescope').load_extension('fzf')
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', '<leader>s', ":source $HOME/.config/nvim/init.lua")
+vim.keymap.set('n', '<leader>s', ":vsplit<CR>")
+vim.keymap.set('n', '<leader><leader>', "<C-W>w<CR>")
+
 vim.keymap.set('n', '<leader>f', builtin.find_files, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
 
-vim.cmd("colorscheme catppuccin-macchiato")
+-- vim.cmd("colorscheme catppuccin-macchiato")
+vim.cmd("colorscheme ayu-mirage")
 require('Comment').setup()
+require("mason").setup()
+require("mason-lspconfig").setup {
+    ensure_installed = { "lua_ls", "rust_analyzer" },
+}
+require("lspconfig").lua_ls.setup {}
+require("lspconfig").rust_analyzer.setup {}
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(ev)
+      local opts = {buffer = ev.buf}
+
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+      vim.keymap.set('n', '<leader>r', vim.lsp.buf.rename, opts)
+      vim.keymap.set('n', '<leader>u', vim.lsp.buf.references, opts)
+   end,
+})
+
